c3286c4b28b5d8d154e9b27da9532e26b9bf948b
The_Astro_Coder <153275094+theAstroCoder-01@users.noreply.github.com>
2024-08-10 11:23:00 -0600

    Add files via upload

---
+++ src/main/java/com/Spring/Boot/with/Thymeleaf/MyStore/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.Spring.Boot.with.Thymeleaf.MyStore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
+    }
+}
---
+++ src/test/java/com/Spring/Boot/with/Thymeleaf/MyStore/MyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Boot.with.Thymeleaf.MyStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
